0e0aa7f3d63b58e4b87c4ce3c1974f8bcbecae4d
accyolefelipe <accyole@hotmail.com>
2020-07-02 01:45:15 -0300

    criado o postForm / criados os metodos para rendereizar o form method get , e o saveForm com method post

--- src/main/java/com/projeto/blog/controller/PostController.java
+++ src/main/java/com/projeto/blog/controller/PostController.java
@@ -1,13 +1,18 @@
 package com.projeto.blog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.projeto.blog.model.Post;
 import com.projeto.blog.service.PostService;
@@ -32,6 +37,24 @@ public class PostController {
 		Post post = postService.findById(id);
 		mv.addObject("post", post);
 		return mv;
+	}
+	
+	@RequestMapping(value = "/newpost", method= RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method= RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return "redirect:/posts/newpost";
+		}
+		else {
+			post.setData(LocalDate.now());
+			postService.save(post);
+		}
+		return "redirect:/posts";
+		
 	}
 	
 	
