76c3eaf2145eb6511b95eba0d012c0e2c72c2728
accyolefelipe <accyole@hotmail.com>
2020-07-02 00:31:06 -0300

    criada postDetais.html / criado metodo getById no postService

--- src/main/java/com/projeto/blog/controller/PostController.java
+++ src/main/java/com/projeto/blog/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,6 +23,14 @@ public class PostController {
 		ModelAndView mv = new ModelAndView("posts");
 		List<Post> posts = postService.findAll();
 		mv.addObject("posts", posts);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/posts/{id}", method= RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = postService.findById(id);
+		mv.addObject("post", post);
 		return mv;
 	}
 	
